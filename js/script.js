c12a17c4abd70b10b96a3871858463a87e887698
izaarruda <izabellyarruda1307@gmail.com>
2023-05-17 15:28:30 -0300

    17/05/2023

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function soma(n1, n2) {
+    return n1 + n2
+}
+function multiplicacao (n9, n3) {
+    return n9 * n3
+}
+function subtracao (n5, n4) {
+    return n5 - n4
+}
+function divisao (n6, n7) {
+    return n6 / n7
+}
+function area (){
+    let base = prompt("base")
+    let altura = prompt ("altura")
+    let area
+    area=base*altura
+    alert(area)
+}
